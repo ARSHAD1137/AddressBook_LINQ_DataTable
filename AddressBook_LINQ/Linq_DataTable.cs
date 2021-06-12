054b82df0dc673af99d2c7abe4c79d459a2fde31
Arshad <arshadpinjari666@gmail.com>
2021-06-12 20:11:07 +0530

    Add project files.

---
+++ AddressBook_LINQ/Linq_DataTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace AddressBook_LINQ
+{
+    class Linq_DataTable
+    {
+        public void AddToDataTableDemo()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(int));
+            table.Columns.Add("Email", typeof(string));
+        }
+
+    }
+}
---
+++ AddressBook_LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to Address Book");
+            Linq_DataTable linqToDataTable = new Linq_DataTable();
+        }
+    }
+}
